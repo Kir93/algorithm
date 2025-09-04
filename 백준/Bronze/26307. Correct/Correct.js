ab24d77ce618ba4b4a5e0fa73f4fbf7d83d22fcd
CR Han <kir931028@gmail.com>
2025-09-04 14:56:30 +0900

    [Bronze V] Title: Correct, Time: 104 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+console.log((+input[0] - 9) * 60 + +input[1]);
