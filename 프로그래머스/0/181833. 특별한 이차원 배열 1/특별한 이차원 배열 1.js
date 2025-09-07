8a15df24e4ce3ca76b7c0a43434270ca98932ee5
CR Han <kir931028@gmail.com>
2025-09-07 15:34:54 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.98 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,10 +1,5 @@
 function solution(n) {
-  var answer = [];
-  for (let i = 0; i < n; i++) {
-    answer.push([]);
-    for (let j = 0; j < n; j++) {
-      answer[i].push(+(i === j));
-    }
-  }
-  return answer;
+  return Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (_, j) => +(i === j)),
+  );
 }
