d252a8ea0d8660fa4901c73c5fd14e0ce22c6e43
CR Han <kir931028@gmail.com>
2025-09-17 14:03:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,17 +1,6 @@
 function solution(my_string) {
-  let answer = 0;
-  let convertNum = '';
-
-  my_string.split('').forEach((char) => {
-    if (!isNaN(char)) convertNum += char;
-    else {
-      if (convertNum) {
-        answer += parseInt(convertNum, 10);
-        convertNum = '';
-      }
-    }
-  });
-
-  if (convertNum) answer += parseInt(convertNum, 10);
-  return answer;
+  return my_string
+    .replace(/[A-z]/g, ' ')
+    .split(' ')
+    .reduce((acc, cur) => acc + +cur, 0);
 }
