5225d44accd97b5f61889ce20ce4f631e28463d4
CR Han <kir931028@gmail.com>
2025-09-18 19:27:58 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  return s
+    .split(' ')
+    .reduce(
+      (acc, cur, i, arr) =>
+        cur === 'Z' ? acc - Number(arr[i - 1]) : acc + Number(cur),
+      0,
+    );
+}
