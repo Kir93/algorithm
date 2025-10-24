d0c5542e866eacddb8e653e5600a2704eb8813f8
CR Han <kir931028@gmail.com>
2025-10-24 20:34:00 +0900

    [Bronze V] Title: Multiply, Time: 120 ms, Memory: 9612 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.js
+++ 백준/Bronze/22193. Multiply/Multiply.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const a = parseInt(input[1]);
-const b = parseInt(input[2]);
+const a = BigInt(input[1]);
+const b = BigInt(input[2]);
 
-console.log(a * b);
+console.log((a * b).toString());
