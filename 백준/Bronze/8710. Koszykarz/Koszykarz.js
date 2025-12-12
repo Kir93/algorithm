66d73ef09ad93543d4e3bbe13e8219197bf2a6c2
CR Han <kir931028@gmail.com>
2025-12-12 18:35:15 +0900

    [Bronze IV] Title: Koszykarz, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/8710. Koszykarz/Koszykarz.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [k, w, m] = input[0].split(' ').map(Number);
+
+console.log(Math.ceil((w - k) / m));
