468c52bb6a148a2c4edb1181faab8b1d70d4e1ee
CR Han <kir931028@gmail.com>
2025-12-21 09:56:56 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+console.log(3 + parseInt(input[0]) + parseInt(input[1]));
