2f92fe899b49bdb8d8cd6955718bc6003c58823e
CR Han <kir931028@gmail.com>
2025-12-28 09:53:34 +0900

    [Bronze II] Title: 방 배정, Time: 100 ms, Memory: 109412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13304. 방 배정/방 배정.py
+++ 백준/Bronze/13304. 방 배정/방 배정.py
@@ -1,8 +1,22 @@
+import math
+
 N, K = map(int, input().split())
-student = [[0] * 3 for _ in range(2)]
+student = [0, 0, 0, 0, 0]
 
 for _ in range(N):
     S, Y = map(int, input().split())
-    student[S][(Y - 1) // 2] += 1
-
-print(sum((cnt + K - 1) // K for row in student for cnt in row))
+    if Y == 1 or Y == 2:
+        student[0] += 1
+    elif S == 0 and (Y == 3 or Y == 4):
+        student[1] += 1
+    elif S == 1 and (Y == 3 or Y == 4):
+        student[2] += 1
+    elif S == 0 and (Y == 5 or Y == 6):
+        student[3] += 1
+    else:
+        student[4] += 1
+        
+room = 0
+for i in student:
+    room += math.ceil(i / K)
+print(room)
