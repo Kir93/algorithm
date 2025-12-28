3cdad58615c6e5be2724f693a68382f81fad149c
CR Han <kir931028@gmail.com>
2025-12-28 09:55:34 +0900

    [Bronze II] Title: 방 배정, Time: 100 ms, Memory: 9608 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13304. 방 배정/방 배정.js
+++ 백준/Bronze/13304. 방 배정/방 배정.js
@@ -3,13 +3,26 @@ const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [N, K] = input[0].split(' ').map(Number);
-const student = Array.from({ length: 2 }, () => [0, 0, 0]);
+const student = [0, 0, 0, 0, 0];
 
 for (let i = 1; i <= N; i++) {
   const [S, Y] = input[i].split(' ').map(Number);
-  student[S][Math.floor((Y - 1) / 2)]++;
+  if (Y === 1 || Y === 2) {
+    student[0] += 1;
+  } else if (S === 0 && (Y === 3 || Y === 4)) {
+    student[1] += 1;
+  } else if (S === 1 && (Y === 3 || Y === 4)) {
+    student[2] += 1;
+  } else if (S === 0 && (Y === 5 || Y === 6)) {
+    student[3] += 1;
+  } else {
+    student[4] += 1;
+  }
 }
 
-const room = student.flat().reduce((sum, cnt) => sum + Math.ceil(cnt / K), 0);
+let room = 0;
+for (let i = 0; i < student.length; i++) {
+  room += Math.ceil(student[i] / K);
+}
 
 console.log(room);
