cae36fbada7043a654c1215c462bf4dbae8a4866
CR Han <kir931028@gmail.com>
2026-01-19 15:25:40 +0900

    [Bronze III] Title: Square Pasture, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/14173. Square Pasture/Square Pasture.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
+const [x3, y3, x4, y4] = input[1].split(' ').map(Number);
+
+const x = Math.max(x2, x4) - Math.min(x1, x3);
+const y = Math.max(y2, y4) - Math.min(y1, y3);
+
+console.log(Math.max(x, y) ** 2);
