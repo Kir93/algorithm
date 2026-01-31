3e2b35bc9d890ecefff1f1c7229b69747664e0fb
CR Han <kir931028@gmail.com>
2026-01-31 23:38:41 +0900

    [Bronze IV] Title: X に最も近い値 (The Nearest Value), Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/18414. X に最も近い値 （The Nearest Value）/X に最も近い値 （The Nearest Value）.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const numbers = input[0]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+console.log(numbers[1]);
