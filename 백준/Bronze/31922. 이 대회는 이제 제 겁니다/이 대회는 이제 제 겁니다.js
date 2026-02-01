ffc64b593c249321211133a4720043e273d8a744
CR Han <kir931028@gmail.com>
2026-02-01 13:22:06 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [A, P, C] = input[0].split(' ').map(Number);
+
+console.log(Math.max(A + C, P));
