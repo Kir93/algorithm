84b8247c4dd827a2e5d41ac7d6b2181e0f5860a4
CR Han <kir931028@gmail.com>
2026-03-02 15:21:55 +0900

    [Bronze IV] Title: 특별한 가지, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [N, M, K] = input.map(Number);
+
+console.log((M / N) * K);
