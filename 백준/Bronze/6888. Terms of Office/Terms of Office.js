f9d3565bdc2b5b5ffd29e9b60a3c23fe6b30dfa4
Kir93 <kir931028@gmail.com>
2026-03-14 09:19:06 +0900

    fix : 소수점 처리 수정

--- 백준/Bronze/6888. Terms of Office/Terms of Office.js
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [start, end] = input.map(Number);
 
-const cnt = (end - start) / 60;
+const cnt = Math.floor((end - start) / 60);
 
 for (let i = 0; i <= cnt; i++) {
   console.log(`All positions change in year ${start + i * 60}`);
