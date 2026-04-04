677781c9fd9b75b9a0a792306b6f905e332af8da
CR Han <kir931028@gmail.com>
2026-04-04 14:17:46 +0900

    [Bronze III] Title: 노 땡스!, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+cards = list(map(int, input().split())) + [0] 
+
+result = []
+tmp = []
+for i in range(n) :
+    tmp.append(cards[i])
+
+    if (cards[i+1] - cards[i]) != 1 :
+        result.append(tmp)
+        tmp = []
+
+total = 0
+for j in result :
+    total += j[0]
+
+print(total)
