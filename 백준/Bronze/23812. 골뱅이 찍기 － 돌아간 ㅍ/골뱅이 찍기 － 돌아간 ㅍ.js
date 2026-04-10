8708d7a9ca285c39e3081dd8060ab5725bd39149
CR Han <kir931028@gmail.com>
2026-04-10 10:57:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 112 ms, Memory: 10576 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+
+for (let i = 0; i < n; i++) {
+  console.log('@'.repeat(n) + ' '.repeat(3 * n) + '@'.repeat(n));
+}
+
+for (let i = 0; i < n; i++) {
+  console.log('@'.repeat(5 * n));
+}
+
+for (let i = 0; i < n; i++) {
+  console.log('@'.repeat(n) + ' '.repeat(3 * n) + '@'.repeat(n));
+}
+
+for (let i = 0; i < n; i++) {
+  console.log('@'.repeat(5 * n));
+}
+
+for (let i = 0; i < n; i++) {
+  console.log('@'.repeat(n) + ' '.repeat(3 * n) + '@'.repeat(n));
+}
